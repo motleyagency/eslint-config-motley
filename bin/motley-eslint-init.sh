6f7a9fa09035f1cc9dbd9103bc7c17fc9ebc6998
Fredrik Boström <fredrik@fredrikbostrom.net>
2017-04-12 16:44:59 +0300

    Add check for yarn.lock and use yarn to install if available

--- bin/motley-eslint-init.sh
+++ bin/motley-eslint-init.sh
@@ -1,2 +1,12 @@
-PKG=eslint-config-airbnb;
-npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG@latest"
+PKG=eslint-config-airbnb
+
+# Default to installing using npm
+INSTALL="npm install --save-dev";
+
+# Is this a yarn project, then install using yarn
+if [[ -f $(PWD)/yarn.lock ]]
+then
+  INSTALL="yarn add --dev"
+fi
+
+npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs $INSTALL "$PKG@latest"
