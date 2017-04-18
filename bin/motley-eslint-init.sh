eafb99a12d37660e1b4b768ea736a9181150477d
Fredrik Boström <fredrik@fredrikbostrom.net>
2017-04-18 14:22:10 +0300

    Remove postinstall

--- bin/motley-eslint-init.sh
+++ bin/motley-eslint-init.sh
@@ -3,7 +3,7 @@
 PKG=eslint-config-airbnb
 
 # Default to installing using npm
-INSTALL="npm install --save-dev";
+INSTALL="npm install --save-dev"
 
 # Is this a yarn project and yarn is found in PATH, then install using yarn
 if [[ -f $(PWD)/yarn.lock && -x $(which yarn) ]]
@@ -11,4 +11,4 @@ then
   INSTALL="yarn add --dev"
 fi
 
-npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs "$INSTALL" "$PKG@latest"
+npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs $INSTALL "$PKG@latest"
