5832b3ec1fa33073b1ea1746c47c3ca27f9d5f98
petetnt <pete.a.nykanen@gmail.com>
2023-12-05 16:00:28 +0200

    chore: more fixes to try to get Verdaccio back up and running
    Signed-off-by: petetnt <pete.a.nykanen@gmail.com>

--- scripts/publish-to-verdaccio.sh
+++ scripts/publish-to-verdaccio.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
 
 set -e
-local_registry="http://0.0.0.0:4873"
+local_registry="http://localhost:4873"
+npm set registry $local_registry
 
 # start local registry
-tmp_registry_log=`mktemp`
-sh -c "nohup ./node_modules/.bin/verdaccio --config .config/verdaccio/config.yaml &>$tmp_registry_log &"
-# login so we can publish package
-echo "Logging in"
-sh -c "./node_modules/.bin/npm-auth-to-token -u motley -p motley -e it@motley.fi -r $local_registry"
+sh -c "./node_modules/.bin/verdaccio --config .config/verdaccio/config.yaml --listen $local_registry &"
 
-echo "Publishing package locally"
-cp .npmrc packages/eslint-config-motley-typescript/.npmrc
+until $(curl --output /dev/null --silent --head --fail $local_registry); do
+    printf '.'
+    sleep 1
+done
+
+echo "Publishing locally"
+cp .config/verdaccio/.npmrc packages/eslint-config-motley-typescript/.npmrc
 
 # Run npm command
 echo "Publishing eslint-config-motley-typescript locally"
-cd ../eslint-config-motley-typescript && sh -c "npm publish --registry $local_registry"
+cd packages/eslint-config-motley-typescript && sh -c "npm publish --registry $local_registry"
 cd ../../
-
-cp .npmrc __fixtures__/ts/.npmrc
+cp .config/verdaccio/.npmrc __fixtures__/ts/.npmrc
 
 echo "Installing eslint-config-motley-typescript to fixtures/ts"
 (
   cd __fixtures__/ts
   export PKG=eslint-config-motley-typescript
-  npm_config_yes=true npx install-peerdeps --dev "$PKG@latest" --extra-args="--registry $local_registry"
-  npm install --registry https://registry.npmjs.org react react-dom
+  npx install-peerdeps --dev "$PKG@latest" --extra-args="--registry $local_registry --maxsockets 1"
 )
 
 echo "Finished!"
-
